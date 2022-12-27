f41bda8fe6964c730180a45c56839b5b46e1411d
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-27 16:49:26 -0500

    completed exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("dad", true)]
+        [InlineData("dog", false)]
+        [InlineData("pop", true)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            WordSmith challenger = new WordSmith();
+            var actual = challenger.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
